20aa333f7a88449d946e4a82e87e52b3815ce17b
Diptanshu <sinha.diptanshu10@gmail.com>
2020-04-15 22:43:49 +0530

    Plotting for each state with one parameter being tuned using 6 values in grid search manner

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from fbprophet.plot import plot_forecast_component, plot_yearly
 import matplotlib.pyplot as plt
 import datetime
 import fbprophet
@@ -18,19 +19,25 @@ for j in states:
     # Prophet requires columns ds (Date) and y (value)
     df2 = df2.rename(columns={'Date': 'ds', 'Confirmed': 'y'})
     df_train = df2.loc[df2["ds"]<"2020-04-09"]
-    df_test  = df2.loc[df2["ds"]>="2020-04-09"]
+    df_test  = df2.loc[df2["ds"]>="2020-02-09"]
     #gm['y'] = gm['y'] / 1e9
     # Make the prophet model and fit on the data
-    df_prophet = fbprophet.Prophet(changepoint_prior_scale=0.99)
-    df_prophet.add_regressor('Cured')
-    df_prophet.add_regressor('Deaths')
-    df_prophet.fit(df_train)
-    #df_prophet.fit(df2)
-    # Python
-    #future = df_prophet.make_future_dataframe(periods=10,freq='D')
-    #forecast=df_prophet.predict(future)
-    forecast = df_prophet.predict(df_test.drop(columns='y'))
-    df_prophet.plot(forecast)
-    df_prophet.plot_components(forecast)
-    #df_prophet.plot_components(forecast)
+    changepointVal=[0.15,0.30,0.50,0.70,0.90,0.99]
+    fig = plt.figure(figsize=(50,50))
+    for i in range(len(changepointVal)):
+        df_prophet = fbprophet.Prophet(changepoint_prior_scale=changepointVal[i])
+        df_prophet.add_regressor('Cured')
+        df_prophet.add_regressor('Deaths')
+        df_prophet.fit(df_train)
+        #df_prophet.fit(df2)
+        # Python
+        #future = df_prophet.make_future_dataframe(periods=10,freq='D')
+        #forecast=df_prophet.predict(future)
+        forecast = df_prophet.predict(df_test.drop(columns='y'))
+        ax1 = fig.add_subplot(6,1,i+1)
+        df_prophet.plot(forecast,ax=ax1)
     plt.show()
+        #df_prophet.plot_components(forecast)
+
+    #df_prophet.plot_components(forecast)
+    #plt.show()
